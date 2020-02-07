8673b9b615397493b38c63eeb1b4ca7e7960f47e
Roman Karpov <poma.05@mail.ru>
2020-02-07 17:14:56 +0400

    init

---
+++ Proxy/Proxy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    public class Proxy : Subject
+    {
+        private RealSubject realSubject;
+        public override void Request()
+        {
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+            realSubject.Request();
+        }
+    }
+}
